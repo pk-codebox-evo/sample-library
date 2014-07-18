661e1ee8119ed2896da6ee08262932517274efee
Konstantin Krivopustov <krivopustov@haulmont.com>
2014-07-18 11:27:48 +0400

    Add RunReportAction to book browse.

--- modules/gui/src/com/sample/library/gui/book/BookBrowse.java
+++ modules/gui/src/com/sample/library/gui/book/BookBrowse.java
@@ -6,10 +6,18 @@ package com.sample.library.gui.book;
 
 import java.util.Map;
 import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.components.Button;
+import com.haulmont.reports.gui.actions.RunReportAction;
+
+import javax.inject.Inject;
 
 public class BookBrowse extends AbstractLookup {
 
+    @Inject
+    private Button reportButton;
+
     @Override
     public void init(Map<String, Object> params) {
+        reportButton.setAction(new RunReportAction(this, "report"));
     }
 }
